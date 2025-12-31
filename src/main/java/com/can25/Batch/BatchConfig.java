c5df1fd17829c6e7c7a22db729cb25cfc3e216fb
ismail el ghaba <ismail.elghaba.main@gmail.com>
2025-12-31 02:23:25 +0100

    auto detect extension

--- src/main/java/com/can25/Batch/BatchConfig.java
+++ src/main/java/com/can25/Batch/BatchConfig.java
@@ -1,10 +1,6 @@
 package com.can25.Batch;
 
 import com.can25.Dto.SpectatorDTO;
-import com.can25.Entity.SeatLocation;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -12,72 +8,21 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.json.JacksonJsonObjectReader;
-import org.springframework.batch.item.json.JsonItemReader;
-import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
-import org.springframework.batch.item.xml.StaxEventItemReader;
-import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.oxm.xstream.XStreamMarshaller;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class BatchConfig {
 
-        @Value("${source.type:json}")
-        private String sourceType;
+        @Value("${input.file}")
+        private String inputFile;
 
         @Bean
-        public JsonItemReader<SpectatorDTO> jsonItemReader() {
-                ObjectMapper objectMapper = new ObjectMapper();
-                objectMapper.registerModule(new JavaTimeModule());
-                objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-                JacksonJsonObjectReader<SpectatorDTO> jsonObjectReader = new JacksonJsonObjectReader<>(
-                                SpectatorDTO.class);
-                jsonObjectReader.setMapper(objectMapper);
-
-                return new JsonItemReaderBuilder<SpectatorDTO>()
-                                .name("jsonSpectatorReader")
-                                .jsonObjectReader(jsonObjectReader)
-                                .resource(new ClassPathResource("spectators_json.json"))
-                                .strict(false)
-                                .build();
-        }
-
-        @Bean
-        public StaxEventItemReader<SpectatorDTO> xmlItemReader() {
-                Map<String, Class<?>> aliases = new HashMap<>();
-                aliases.put("spectatorEntry", SpectatorDTO.class);
-                aliases.put("seatLocation", SeatLocation.class);
-
-                XStreamMarshaller marshaller = new XStreamMarshaller();
-                marshaller.setAliases(aliases);
-
-                // Allow security for XStream
-                marshaller.getXStream().allowTypes(new Class[] { SpectatorDTO.class, SeatLocation.class });
-
-                return new StaxEventItemReaderBuilder<SpectatorDTO>()
-                                .name("xmlSpectatorReader")
-                                .resource(new ClassPathResource("spectators_xml.xml"))
-                                .addFragmentRootElements("spectatorEntry")
-                                .unmarshaller(marshaller)
-                                .build();
-        }
-
-        @Bean
-        public MainItemReader mainItemReader(
-                        @Qualifier("jsonItemReader") ItemReader<SpectatorDTO> jsonReader,
-                        @Qualifier("xmlItemReader") ItemReader<SpectatorDTO> xmlReader) {
-                return new MainItemReader(jsonReader, xmlReader, sourceType);
+        public MainItemReader mainItemReader() {
+                return new MainItemReader(new ClassPathResource(inputFile));
         }
 
         @Bean
--- src/main/java/com/can25/Batch/MainItemReader.java
+++ src/main/java/com/can25/Batch/MainItemReader.java
@@ -1,71 +1,112 @@
 package com.can25.Batch;
 
 import com.can25.Dto.SpectatorDTO;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemStream;
-import org.springframework.batch.item.ItemStreamException;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
+import com.can25.Entity.SeatLocation;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.batch.item.*;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.core.io.Resource;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainItemReader implements ItemReader<SpectatorDTO>, ItemStream {
 
-    private final ItemReader<SpectatorDTO> jsonReader;
-    private final ItemReader<SpectatorDTO> xmlReader;
-    private final String sourceType;
+    private final Resource resource;
+    private ItemReader<SpectatorDTO> delegate;
 
-    public MainItemReader(ItemReader<SpectatorDTO> jsonReader, ItemReader<SpectatorDTO> xmlReader, String sourceType) {
-        this.jsonReader = jsonReader;
-        this.xmlReader = xmlReader;
-        this.sourceType = sourceType;
+    public MainItemReader(Resource resource) {
+        this.resource = resource;
+    }
+
+    private void initializeDelegate() {
+        if (delegate != null) {
+            return;
+        }
+
+        String filename = resource.getFilename();
+        if (filename == null) {
+            throw new IllegalArgumentException("Resource filename cannot be null");
+        }
+
+        if (filename.toLowerCase().endsWith(".json")) {
+            this.delegate = createJsonReader();
+        } else if (filename.toLowerCase().endsWith(".xml")) {
+            this.delegate = createXmlReader();
+        } else {
+            throw new IllegalArgumentException("Unsupported file extension: " + filename);
+        }
+    }
+
+    private ItemReader<SpectatorDTO> createJsonReader() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        JacksonJsonObjectReader<SpectatorDTO> jsonObjectReader = new JacksonJsonObjectReader<>(SpectatorDTO.class);
+        jsonObjectReader.setMapper(objectMapper);
+
+        return new JsonItemReaderBuilder<SpectatorDTO>()
+                .name("jsonSpectatorReader")
+                .jsonObjectReader(jsonObjectReader)
+                .resource(resource)
+                .strict(false)
+                .build();
+    }
+
+    private ItemReader<SpectatorDTO> createXmlReader() {
+        Map<String, Class<?>> aliases = new HashMap<>();
+        aliases.put("spectatorEntry", SpectatorDTO.class);
+        aliases.put("seatLocation", SeatLocation.class);
+
+        XStreamMarshaller marshaller = new XStreamMarshaller();
+        marshaller.setAliases(aliases);
+        marshaller.getXStream().allowTypes(new Class[] { SpectatorDTO.class, SeatLocation.class });
+
+        return new StaxEventItemReaderBuilder<SpectatorDTO>()
+                .name("xmlSpectatorReader")
+                .resource(resource)
+                .addFragmentRootElements("spectatorEntry")
+                .unmarshaller(marshaller)
+                .build();
     }
 
     @Override
     public SpectatorDTO read()
-            throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-        if ("xml".equalsIgnoreCase(sourceType)) {
-            return xmlReader.read();
-        } else {
-            return jsonReader.read();
+            throws Exception {
+        if (delegate == null) {
+            initializeDelegate();
+        }
+        return delegate.read();
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        if (delegate == null) {
+            initializeDelegate();
+        }
+        if (delegate instanceof ItemStream) {
+            ((ItemStream) delegate).open(executionContext);
         }
     }
 
     @Override
-    public void open(org.springframework.batch.item.ExecutionContext executionContext) throws ItemStreamException {
-        if ("xml".equalsIgnoreCase(sourceType)) {
-            if (xmlReader instanceof ItemStream) {
-                ((ItemStream) xmlReader).open(executionContext);
-            }
-        } else {
-            if (jsonReader instanceof ItemStream) {
-                ((ItemStream) jsonReader).open(executionContext);
-            }
-        }
-    }
-
-    @Override
-    public void update(org.springframework.batch.item.ExecutionContext executionContext) throws ItemStreamException {
-        if ("xml".equalsIgnoreCase(sourceType)) {
-            if (xmlReader instanceof ItemStream) {
-                ((ItemStream) xmlReader).update(executionContext);
-            }
-        } else {
-            if (jsonReader instanceof ItemStream) {
-                ((ItemStream) jsonReader).update(executionContext);
-            }
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+        // delegate should be initialized by open()
+        if (delegate instanceof ItemStream) {
+            ((ItemStream) delegate).update(executionContext);
         }
     }
 
     @Override
     public void close() throws ItemStreamException {
-        if ("xml".equalsIgnoreCase(sourceType)) {
-            if (xmlReader instanceof ItemStream) {
-                ((ItemStream) xmlReader).close();
-            }
-        } else {
-            if (jsonReader instanceof ItemStream) {
-                ((ItemStream) jsonReader).close();
-            }
+        if (delegate instanceof ItemStream) {
+            ((ItemStream) delegate).close();
         }
     }
 }
